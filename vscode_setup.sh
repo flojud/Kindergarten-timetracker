5be993cc86beca6b0303fd9aa77fba77ace22dbb
Florian Jud <florian.jud@mercedes-benz.com>
2022-10-12 15:36:31 +0200

    Added vscode setup

---
+++ vscode_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension esbenp.prettier-vscode
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension burkeholland.simple-react-snippets
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension mgmcdermott.vscode-language-babel
